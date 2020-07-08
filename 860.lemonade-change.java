34ebaa9859a82e579e570a6b472d39805a9871e5
houlx <houlxssdut@gmail.com>
2020-07-09 03:09:15 +0900

    860

---
+++ 860.lemonade-change.java
@@ -0,0 +1,135 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * @lc app=leetcode id=860 lang=java
+ *
+ * [860] Lemonade Change
+ *
+ * https://leetcode.com/problems/lemonade-change/description/
+ *
+ * algorithms
+ * Easy (51.47%)
+ * Likes:    579
+ * Dislikes: 77
+ * Total Accepted:    52.5K
+ * Total Submissions: 102K
+ * Testcase Example:  '[5,5,5,10,20]'
+ *
+ * At a lemonade stand, each lemonade costs $5. 
+ * 
+ * Customers are standing in a queue to buy from you, and order one at a time
+ * (in the order specified by bills).
+ * 
+ * Each customer will only buy one lemonade and pay with either a $5, $10, or
+ * $20 bill.  You must provide the correct change to each customer, so that the
+ * net transaction is that the customer pays $5.
+ * 
+ * Note that you don't have any change in hand at first.
+ * 
+ * Return true if and only if you can provide every customer with correct
+ * change.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [5,5,5,10,20]
+ * Output: true
+ * Explanation: 
+ * From the first 3 customers, we collect three $5 bills in order.
+ * From the fourth customer, we collect a $10 bill and give back a $5.
+ * From the fifth customer, we give a $10 bill and a $5 bill.
+ * Since all customers got correct change, we output true.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [5,5,10]
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [10,10]
+ * Output: false
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [5,5,10,10,20]
+ * Output: false
+ * Explanation: 
+ * From the first two customers in order, we collect two $5 bills.
+ * For the next two customers in order, we collect a $10 bill and give back a
+ * $5 bill.
+ * For the last customer, we can't give change of $15 back because we only have
+ * two $10 bills.
+ * Since not every customer received correct change, the answer is false.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 0 <= bills.length <= 10000
+ * bills[i] will be either 5, 10, or 20.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        Deque<Integer> q5 = new ArrayDeque<>();
+        Deque<Integer> q10 = new ArrayDeque<>();
+
+        for (int bill : bills) {
+            switch (bill) {
+                case 5:
+                    q5.add(bill);
+                    break;
+                case 10:
+                    if (q5.isEmpty())
+                        return false;
+                    q5.remove();
+                    q10.add(bill);
+                    break;
+                case 20:
+                    if (q5.isEmpty())
+                        return false;
+                    if (!q10.isEmpty()) {
+                        q10.remove();
+                        q5.remove();
+                    } else {
+                        int cnt = 0;
+                        while (!q5.isEmpty() && cnt < 3) {
+                            q5.remove();
+                            cnt++;
+                        }
+                        if (cnt < 3) {
+                            return false;
+                        }
+                    }
+                default:
+                    break;
+            }
+        }
+
+        return true;
+    }
+}
+// @lc code=end
