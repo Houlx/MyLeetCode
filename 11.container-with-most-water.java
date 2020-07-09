f8f124e7cbcea1499d7f1c70d5ffd4dea23ff245
houlx <houlxssdut@gmail.com>
2020-07-09 13:51:14 +0900

    11

---
+++ 11.container-with-most-water.java
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (50.42%)
+ * Likes:    6089
+ * Dislikes: 582
+ * Total Accepted:    655.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ * 
+ * Note: You may not slant the container and n is at least 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49. 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+
+// @lc code=start
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int water = Integer.MIN_VALUE;
+//         int tmpWater;
+//         for (int i = 0; i < height.length - 1; i++) {
+//             for (int j = i + 1; j < height.length; j++) {
+//                 tmpWater = Math.min(height[i], height[j]) * (j - i);
+//                 if (tmpWater > water) {
+//                     water = tmpWater;
+//                 }
+//             }
+//         }
+//         return water;
+//     }
+
+// }
+class Solution {
+    public int maxArea(int[] height) {
+        int water = Integer.MIN_VALUE;
+        int l = 0;
+        int r = height.length - 1;
+        int tmpWater = 0;
+        while (l < r) {
+            tmpWater = (r - l) * Math.min(height[l], height[r]);
+            if (water < tmpWater) {
+                water = tmpWater;
+            }
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return water;
+    }
+
+}
+// @lc code=end
