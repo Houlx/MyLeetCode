c75c3b7dac57509e34d56ba41ae32cd3b2ec0dc4
houlx <houlxssdut@gmail.com>
2020-07-11 04:34:22 +0900

    82

---
+++ 24.swap-nodes-in-pairs.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (49.92%)
+ * Likes:    2252
+ * Dislikes: 169
+ * Total Accepted:    468.8K
+ * Total Submissions: 937.8K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * 
+ * You may not modify the values in the list's nodes, only nodes itself may be
+ * changed.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * 
+ * 
+ */
+
+// @lc code=start
+
+//* Definition for singly-linked list.
+// class ListNode {
+//     int val;
+//     ListNode next;
+
+//     ListNode() {
+//     }
+
+//     ListNode(int val) {
+//         this.val = val;
+//     }
+
+//     ListNode(int val, ListNode next) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+    }
+}
+// @lc code=end
---
+++ 82.remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=82 lang=java
+ *
+ * [82] Remove Duplicates from Sorted List II
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+ *
+ * algorithms
+ * Medium (36.43%)
+ * Likes:    1663
+ * Dislikes: 107
+ * Total Accepted:    249.3K
+ * Total Submissions: 683.5K
+ * Testcase Example:  '[1,2,3,3,4,4,5]'
+ *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * Return the linked list sorted as well.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ * 
+ * 
+ */
+
+// @lc code=start
+//
+// //* Definition for singly-linked list.
+// class ListNode {
+//     int val;
+//     ListNode next;
+
+//     ListNode() {
+//     }
+
+//     ListNode(int val) {
+//         this.val = val;
+//     }
+
+//     ListNode(int val, ListNode next) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+
+// */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1, head);
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode p = head, q = head, pre = dummy;
+
+        int flag = 0;
+
+        while (pre != null && p != null && q != null) {
+            if (q.next != null && q.next.val == q.val) {
+                flag = 1;
+                q = q.next;
+            } else {
+                q = q.next;
+                if (flag != 1)
+                    pre = pre.next;
+                pre.next = q;
+                flag = 0;
+            }
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
