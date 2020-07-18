108274e870969ee2c2844cf09d3dbfca9e3a6d8c
houlx <houlxssdut@gmail.com>
2020-07-19 00:55:25 +0900

    49

---
+++ 49.group-anagrams.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=49 lang=java
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (56.26%)
+ * Likes:    3591
+ * Dislikes: 185
+ * Total Accepted:    693.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ * 
+ * Note:
+ * 
+ * 
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] cs = s.toCharArray();
+            Arrays.sort(cs);
+            String key = String.valueOf(cs);
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+}
+// @lc code=end
