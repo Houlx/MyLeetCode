49431d0c6ceb629388230722e4b8ff5714bac972
houlx <houlxssdut@gmail.com>
2020-09-12 23:59:47 +0900

    120-On^2 solution

---
+++ 120.triangle.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=120 lang=java
+ *
+ * [120] Triangle
+ *
+ * https://leetcode.com/problems/triangle/description/
+ *
+ * algorithms
+ * Medium (43.71%)
+ * Likes:    2228
+ * Dislikes: 261
+ * Total Accepted:    260.2K
+ * Total Submissions: 584.6K
+ * Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+ *
+ * Given a triangle, find the minimum path sum from top to bottom. Each step
+ * you may move to adjacent numbers on the row below.
+ * 
+ * For example, given the following triangle
+ * 
+ * 
+ * [
+ * ⁠    [2],
+ * ⁠   [3,4],
+ * ⁠  [6,5,7],
+ * ⁠ [4,1,8,3]
+ * ]
+ * 
+ * 
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ * 
+ * Note:
+ * 
+ * Bonus point if you are able to do this using only O(n) extra space, where n
+ * is the total number of rows in the triangle.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int len = triangle.size();
+        int[][] cost = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            cost[len - 1][i] = triangle.get(len - 1).get(i);
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                cost[i][j] = triangle.get(i).get(j) + Math.min(cost[i + 1][j], cost[i + 1][j + 1]);
+            }
+        }
+        return cost[0][0];
+    }
+}
+// @lc code=end
