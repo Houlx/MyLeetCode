8ae7c9e6020c43ea10e394a4e6335546575fec1a
houlx <houlxssdut@gmail.com>
2020-09-13 03:58:50 +0900

    28

--- 28.implement-str-str.java
+++ 28.implement-str-str.java
@@ -45,7 +45,19 @@
 // @lc code=start
 class Solution {
     public int strStr(String haystack, String needle) {
-
+        if ("".equals(needle))
+            return 0;
+        if (haystack.length() < needle.length())
+            return -1;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j))
+                    break;
+                if (j == needle.length() - 1)
+                    return i;
+            }
+        }
+        return -1;
     }
 }
 // @lc code=end
